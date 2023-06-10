b065ce865e4f03bf5e01ba5c39b82beb4d6fdd18
Minhajrabbani <minhajrobbani@gmail.com>
2023-06-10 06:28:16 +0300

    task10B

---
+++ testimonials.js
@@ -0,0 +1,94 @@
+// const testimonialData = [
+//   {
+//     author: "pasukan orange",
+//     quote: "Jagalah Kebersihan",
+//     image:
+//       "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     rating: 5,
+//   },
+//   {
+//     author: "Kucing Gila",
+//     quote: "Keren cuys!!",
+//     image:
+//       "https://images.pexels.com/photos/270360/pexels-photo-270360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     rating: 4,
+//   },
+//   {
+//     author: "Pak Marves",
+//     quote: "The Best Pelayanannya!",
+//     image:
+//       "https://images.pexels.com/photos/811587/pexels-photo-811587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     rating: 3,
+//   },
+//   {
+//     author: "Coldplay",
+//     quote: "War Tikettt!",
+//     image:
+//       "https://images.pexels.com/photos/930530/pexels-photo-930530.jpeg",
+//     rating: 2,
+//   },
+//   {
+//     author: "Banteng Merah",
+//     quote: "Penguasa Prindapan!",
+//     image:
+//       "https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     rating: 4,
+//   },
+//   {
+//     author: "Pak Jarwo",
+//     quote: "Khayalan!",
+//     image:
+//       "https://images.pexels.com/photos/2565919/pexels-photo-2565919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     rating: 5,
+//   },
+// ];
+
+// function allTestimonials() {
+//   let testimonialHTML = "";
+
+//   testimonialData.forEach(function (item) {
+//     testimonialHTML += `<div class="testimonial">
+//                             <img
+//                                 src="${item.image}"
+//                                 class="profile-testimonial"
+//                             />
+//                             <p class="quote">${item.quote}</p>
+//                             <p class="author">- ${item.author}</p>
+//                             <p class="author">${item.rating} <i class="fa-solid fa-star"></i></p>
+//                         </div>
+//         `;
+//   });
+
+//   document.getElementById("testimonials").innerHTML = testimonialHTML;
+// }
+
+// allTestimonials();
+
+// function filterTestimonials(rating) {
+//   let testimonialHTML = "";
+
+//   const testimonialFiltered = testimonialData.filter(function (item) {
+//     return item.rating === rating;
+//   });
+
+//    console.log(testimonialFiltered);
+
+//   if (testimonialFiltered.length === 0) {
+//     testimonialHTML += `<h1>Data not found!</h1>`;
+//   } else {
+//     testimonialFiltered.forEach(function (item) {
+//       testimonialHTML += `<div class="testimonial">
+//                                 <img
+//                                     src="${item.image}"
+//                                     class="profile-testimonial"
+//                                 />
+//                                 <p class="quote">${item.quote}</p>
+//                                 <p class="author">- ${item.author}</p>
+//                                 <p class="author">${item.rating} <i class="fa-solid fa-star"></i></p>
+//                             </div>
+//                         `;
+//     });
+//   }
+
+//   document.getElementById("testimonials").innerHTML = testimonialHTML;
+// }
